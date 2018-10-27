fdae29b23f7dcbfcfb670714516fe693af4bf836
hivesbee <hivesbee@gmail.com>
2018-10-28 02:26:15 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>distance: {{ distance }}</div>
+    <div>drag position: {{ area }}</div>
+
+    <div class="canvas"
+      @mousedown="mouseDown($event)"
+      @mousemove="mouseMove($event)"
+      @mouseup="mouseUp($event)"
+    >
+      <div class="selection" :style="area" />
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+import { interval } from 'rxjs'
+import { startWith, scan, flatMap, map, mapTo, sample, takeUntil, throttle, repeat } from 'rxjs/operators'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      offsetX: 0,
+      offsetY: 0
+    }
+  },
+  created () {
+    this.$nextTick(() => {
+      const e = document.querySelector('.canvas')
+
+      const rect = e.getBoundingClientRect()
+      this.offsetX = rect.left + window.pageXOffset
+      this.offsetY = rect.top + window.pageYOffset
+    })
+  },
+  observableMethods: {
+    mouseDown: 'mouseDown$',
+    mouseMove: 'mouseMove$',
+    mouseUp: 'mouseUp$'
+  },
+  subscriptions () {
+    return {
+      area: this.mouseDown$.pipe(flatMap((e0) => {
+        const x0 = e0.pageX - this.offsetX
+        const y0 = e0.pageY - this.offsetY
+
+        return this.mouseMove$.pipe(
+          throttle(() => interval(10)),
+          map((e1) => {
+            e1.preventDefault()
+            const x1 = e1.pageX - this.offsetX
+            const y1 = e1.pageY - this.offsetY
+
+            const dx = x1 - x0
+            const dy = y1 - y0
+
+            return {
+              top: `${dy >= 0 ? y0 : y1}px`,
+              left: `${dx >= 0 ? x0 : x1}px`,
+              width: `${dx >= 0 ? dx : -dx}px`,
+              height: `${dy >= 0 ? dy : -dy}px`
+            }
+          }
+        ))
+      }))
+      .pipe(
+        takeUntil(this.mouseUp$),
+        repeat()
+      ),
+      distance: this.mouseMove$.pipe(
+        startWith(0),
+        scan((_, e) => ({
+          a: e,
+          x: e.offsetX,
+          y: e.offsetY
+        }))
+      )
+    }
   }
 }
 </script>
 
 <style>
 #app {
+  position: relative;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 200px;
 }
+
+.canvas {
+  position: relative;
+  width: 600px;
+  height: 400px;
+  background-color: #F9F9F9;
+  border: solid 1px #AAAAAA;
+}
+
+.selection {
+  position: absolute;
+  display: block;
+
+
+  border: dashed 1px black;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRx from 'vue-rx'
+
+Vue.use(VueRx)
 
 Vue.config.productionTip = false
 
